dd22c10ae15f0760af765faf4d8b2be8e5b7a2ca
Akin <adekoya@gmail.com>
2019-05-03 17:18:13 -0400

    Remove ignored files

--- mongo-seed/export_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mongoexport --db nodeEcommerce --collection orders  --jsonArray --out orders_init.json
-mongoexport --db nodeEcommerce --collection products  --jsonArray --out products_init.json
-mongoexport --db nodeEcommerce --collection users  --jsonArray --out users_init.json
